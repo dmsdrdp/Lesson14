285e0a8bd8b8344aa8a4ae9b0e0ef1630207584f
Dmitriy <Dmitriy@DESKTOP-8IVVVVB>
2022-10-26 23:47:20 +0300

    Добавьте файлы проекта.

---
+++ Task1/Animal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        private string sound;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;    
+        }
+        public Cat(string name, string sound)
+            : base(name)
+        {
+            this.sound = sound;
+        }
+        public override void Say()
+        {
+            Console.WriteLine(sound);
+        }
+    }
+
+    class Dog : Animal
+    {
+        private string sound;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public string Sound
+        {
+            get => sound;
+        }
+        public Dog(string name, string sound)
+            : base(name)
+        {
+            this.sound = sound;
+        }
+        public override void Say()
+        {
+            Console.WriteLine(sound);
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нажмите 1 - кошка \nНажмите 2 - собака");
+            Cat cat = new Cat("Кошка", "Мяу");
+            Dog dog = new Dog("Собака", "Гав");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                cat.Say();
+                Console.WriteLine("Нажмите 1 - свойсва");
+                int options = Convert.ToInt32(Console.ReadLine());
+                if (options<1 || options>1)
+                {
+                    options = 1;
+                }
+                else if (options == 1)
+                {
+                    cat.ShowInfo();
+                }
+            }
+            else if (choice == 2)
+            {
+                dog.Say();
+                Console.WriteLine("Нажмите 1 - свойсва");
+                int options = Convert.ToInt32(Console.ReadLine());
+                if (options < 1 || options > 1)
+                {
+                    options = 1;
+                }
+                else if (options == 1)
+                {
+                    dog.ShowInfo();
+                }
+            }
+         
+            Console.ReadLine();
+        }
+    }
+}
